b06d53caf91bd2b3ef8f4d8b232a5b05efdedccc
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2015-11-18 20:41:08 +0200

    Added functionality to my spider according to the JSON key-value objects

--- ScrapyTest/spiders/myspider.py
+++ ScrapyTest/spiders/myspider.py
@@ -1,8 +1,9 @@
 from ScrapyTest.items import ScrapytestItem
+from urlparse import urlparse
+from scrapy.http.request import Request
 import scrapy
 import json 
 import os
-
 
 
 class MySpider(scrapy.Spider):
@@ -10,6 +11,7 @@ class MySpider(scrapy.Spider):
         item = {}
         resultsFolder ="./results" 
         allowed_domains = ["mon.grnet.gr"]
+        domain = 'https://mon.grnet.gr'
         start_urls = ["https://mon.grnet.gr/api/rg/"]
          
         def parse(self, response):
@@ -19,39 +21,50 @@ class MySpider(scrapy.Spider):
             MySpider.item[ScrapytestItem.SWITCHES] = jsonresponse[ScrapytestItem.SWITCHES]
             MySpider.item[ScrapytestItem.ROUTERS] = jsonresponse[ScrapytestItem.ROUTERS]
             
+            self.createFile('layer1',self.item)
+            
             for itemsValue in MySpider.item:
-                print MySpider.item[itemsValue]
+                itemLink =  self.domain + MySpider.item[itemsValue]
+          #      yield Request(itemLink, callback = self.parseItem, {'parent': itemsValue})
+                yield Request(itemLink,callback=lambda r:self.parseItem(r, itemsValue))
 
 
-            self.createFile(layer1,self.item)
             return
             
 
 
+        def parseItem(self,response,parent):
+            
+            jsonresponse = json.loads(response.body_as_unicode())
+            self.createFile(parent + 'layer2',jsonresponse)
+ 
+            return
 
+
+
+        def createFile(self,namefile,results):
+
+            try:     
+                folder = os.path.join(self.resultsFolder, namefile + '.dat')
+                file = open(folder, "wb")
+                print folder
+                json.dump(results,file)
+                file.write('\n');
+                file.close()
+            #    for res in results:
+            #        file.write( res + ' , ' + results[res] + '\n');  
+
+            except IOError:
+                print("Could not write to this file!")
+                pass 
+  
         def makedirResults(self):
 
             try:     
                 if not os.path.exists(self.resultsFolder):
                     os.makedirs(self.resultsFolder)
             except OSError:
-                print("Could not make results folder!")
+                print("could not make results folder!")
                 pass 
 
 
-        def createFile(self,namefile,results):
-
-            try:     
-                file = open(self.resultsFolder + namefile + '.dat',"wb")
-
-                for iterat in results:
-                    file.write( "Python is a great language.\nYeah its great!!\n");
-            except IOError:
-                print("Could not write to this file!")
-                pass 
-
-
-            #return item
-            #filename = response.url.split("/")[-2] + '.html'
-            #with open(filename, 'wb') as f:
-            #   f.write(response.body)
