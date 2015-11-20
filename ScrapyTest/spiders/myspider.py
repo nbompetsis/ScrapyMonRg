f44139ca86e9ce2245fb14427d71be17f0e16efc
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2015-11-20 02:17:42 +0200

    Issue in layer3

--- ScrapyTest/spiders/myspider.py
+++ ScrapyTest/spiders/myspider.py
@@ -9,6 +9,8 @@ import os
 class MySpider(scrapy.Spider):
         name = "myspider"
         item = {}
+        item2 = {}
+        item3 = {}
         resultsFolder ="./results" 
         allowed_domains = ["mon.grnet.gr"]
         domain = 'https://mon.grnet.gr'
@@ -21,23 +23,35 @@ class MySpider(scrapy.Spider):
             MySpider.item[ScrapytestItem.SWITCHES] = jsonresponse[ScrapytestItem.SWITCHES]
             MySpider.item[ScrapytestItem.ROUTERS] = jsonresponse[ScrapytestItem.ROUTERS]
             
-            self.createFile('layer1',self.item)
+            #self.createFile('layer1',self.item)
             
             for itemsValue in MySpider.item:
                 itemLink =  self.domain + MySpider.item[itemsValue]
-          #      yield Request(itemLink, callback = self.parseItem, {'parent': itemsValue})
-                yield Request(itemLink,callback=lambda r:self.parseItem(r, itemsValue))
-
-
+                yield Request(itemLink,callback=lambda r:self.parseLayer2(r, itemsValue))
+                
             return
             
 
 
-        def parseItem(self,response,parent):
+        def parseLayer2(self,response,parent):
             
             jsonresponse = json.loads(response.body_as_unicode())
-            self.createFile(parent + 'layer2',jsonresponse)
- 
+            #self.createFile(parent + 'layer2',jsonresponse)
+            self.item2 = jsonresponse
+
+            for item in self.item2:
+                print item
+                #print jsonresponse[jsonItem]
+                link3 = self.domain + self.item2[item]
+                yield Request(link3,callback=lambda r:self.parseLayer3(r, item))
+            return
+
+        def parseLayer3(self,response,parent):
+
+            jsonresponse = json.loads(response.body_as_unicode())
+            print parent 
+            self.createFile(parent + 'layer3',jsonresponse)
+
             return
 
 
@@ -47,7 +61,6 @@ class MySpider(scrapy.Spider):
             try:     
                 folder = os.path.join(self.resultsFolder, namefile + '.dat')
                 file = open(folder, "wb")
-                print folder
                 json.dump(results,file)
                 file.write('\n');
                 file.close()
