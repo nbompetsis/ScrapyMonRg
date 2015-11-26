484eb5389b6f108754b0ced4fdab3703eb3d2ea9
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2015-11-26 16:12:25 +0200

    I completed the parsing of the JSON-like pages. Total 24665 files

--- ScrapyTest/spiders/myspider.py
+++ ScrapyTest/spiders/myspider.py
@@ -13,7 +13,7 @@ class MySpider(scrapy.Spider):
         domains = {}
         statements = {}
         forth_layer = {}
-        resultsFolder ="./results" 
+        results_folder ="./results" 
         allowed_domains = ["mon.grnet.gr"]
         domain = 'https://mon.grnet.gr'
         start_urls = ["https://mon.grnet.gr/api/rg/"]
@@ -22,14 +22,9 @@ class MySpider(scrapy.Spider):
             self.makedirResults()
             self.nodes = json.loads(response.body_as_unicode())
 
-            #self.createFile('layer1',self.item)
-            #rgItem = RgItem()
             for node in self.nodes:
-                #rgItem = RgItem()
-                #rgItem['node'] = node
                 link_node = self.domain + self.nodes[node]
                 request = Request(link_node,callback=self.parseDomain)
-                #request.meta['node'] = items_first
                 request.meta['node'] = node
                 yield request
             
@@ -41,12 +36,8 @@ class MySpider(scrapy.Spider):
 
             node = response.meta['node']
             self.domains = json.loads(response.body_as_unicode())
-            #self.createFile(parent + 'layer2',jsonresponse)
-            #print('Layer1############################################################',rgResponse['node'])
 
             for dom in self.domains:
-                #print item
-                #print jsonresponse[jsonItem
                 link_dom = self.domain + self.domains[dom]
                 request = Request(link_dom,callback=self.parseStatements)
                 request.meta['node'] = node
@@ -79,15 +70,14 @@ class MySpider(scrapy.Spider):
             node = response.meta['node']
             dom = response.meta['domain']
             statement = response.meta['statement']
-            nativeObjects = {}
+            native_objects = {}
 
-            nativeObjects = json.loads(response.body_as_unicode())
-            print '####################################################'
-            print node
-            print dom
-            print statement
-            self.createFile(node + '_' + dom + '_' + statement,nativeObjects)
-            print '1111111111111111111111111111111111111111111111111111111111111111111111111'
+            native_objects = json.loads(response.body_as_unicode())
+            for nat_object in native_objects:
+
+                print '####################################################'
+                self.createFile(node + '_' + dom + '_' + statement + '_' + nat_object,native_objects[nat_object])
+                print '1111111111111111111111111111111111111111111111111111111111111111111111111'
 
             return
 
@@ -95,14 +85,12 @@ class MySpider(scrapy.Spider):
         def createFile(self,namefile,results):
 
             try:     
-                folder = os.path.join(self.resultsFolder, namefile + '.dat')
+                print namefile
+                folder = os.path.join(self.results_folder, namefile + '.dat')
                 file = open(folder, "wb")
                 json.dump(results,file)
                 file.write('\n');
                 file.close()
-            #    for res in results:
-            #        file.write( res + ' , ' + results[res] + '\n');  
-
             except IOError:
                 print("Could not write to this file!")
                 pass 
@@ -110,8 +98,8 @@ class MySpider(scrapy.Spider):
         def makedirResults(self):
 
             try:     
-                if not os.path.exists(self.resultsFolder):
-                    os.makedirs(self.resultsFolder)
+                if not os.path.exists(self.results_folder):
+                    os.makedirs(self.results_folder)
             except OSError:
                 print("could not make results folder!")
                 pass 
