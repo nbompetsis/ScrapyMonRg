4f0ac177b65607c94e08a6650927e29195c277c4
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2015-11-26 21:33:01 +0200

    Update metadata structure and add beanstalkc as job queue:)

--- ScrapyTest/spiders/myspider.py
+++ ScrapyTest/spiders/myspider.py
@@ -1,11 +1,10 @@
-from ScrapyTest.items import ScrapytestItem
+#import ScrapyTest.items
 from urlparse import urlparse
 from scrapy.http.request import Request
-from ScrapyTest.items import RgItem
 import scrapy
 import json 
 import os
-
+import beanstalkc
 
 class MySpider(scrapy.Spider):
         name = "myspider"
@@ -13,12 +12,25 @@ class MySpider(scrapy.Spider):
         domains = {}
         statements = {}
         forth_layer = {}
-        results_folder ="./results" 
+        results_folder = "./results" 
         allowed_domains = ["mon.grnet.gr"]
         domain = 'https://mon.grnet.gr'
         start_urls = ["https://mon.grnet.gr/api/rg/"]
+        beanstalk = ''
+        host_beanstalkd = '195.134.71.194'
          
         def parse(self, response):
+
+            # Connect to Beanstalkd server
+            self.beanstalk = beanstalkc.Connection(host=self.host_beanstalkd, port=11301)
+            
+            # See all tubes:
+            self.beanstalk.tubes()
+
+            # Switch to the default (tube):
+            self.beanstalk.use('default')
+
+
             self.makedirResults()
             self.nodes = json.loads(response.body_as_unicode())
 
@@ -73,14 +85,17 @@ class MySpider(scrapy.Spider):
             native_objects = json.loads(response.body_as_unicode())
             for nat_object in native_objects:
 
-                native_objects[nat_object]['metadata']['upper_layer_infos'] = {"node":node, "domain":dom, "category": statement}
+                native_objects[nat_object]['metadata']['upper_node_info'] = {"name":"node", "value":node}
+                native_objects[nat_object]['metadata']['upper_domain_info'] = {"name":"domain", "value":dom}
+                native_objects[nat_object]['metadata']['upper_category_info'] = {"name":"category", "value":statement}
                 final_object = {}
                 final_object[nat_object] = native_objects[nat_object]
-                #json_final_object = json.dumps(final_object)
+                json_final_object = json.dumps(final_object)
 
                 print '##########################################################################################'
                 self.createFile(node + '_' + dom + '_' + statement + '_' + nat_object,final_object)
-                print final_object
+                self.beanstalk.put(json_final_object)
+                print json_final_object
                 print '##########################################################################################'
 
             return
