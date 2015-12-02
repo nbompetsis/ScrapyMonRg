18b956d9a1e15f33f70d6155e7a53ae9bee73a15
Nikolas Bompetsis <nikolas.mpompetsis@gmail.com>
2015-12-02 14:48:26 +0200

    Added functionaity for the termination of the beanstalkc server

--- ScrapyTest/spiders/myspider.py
+++ ScrapyTest/spiders/myspider.py
@@ -1,6 +1,6 @@
-#import ScrapyTest.items
 from urlparse import urlparse
 from scrapy.http.request import Request
+from scrapy.xlib.pydispatch import dispatcher
 import scrapy
 import json 
 import os
@@ -17,18 +17,22 @@ class MySpider(scrapy.Spider):
         domain = 'https://mon.grnet.gr'
         start_urls = ["https://mon.grnet.gr/api/rg/"]
         beanstalk = ''
-        #host_beanstalkd = '195.134.71.194'
-         
+        host_beanstalkd = '127.0.0.1'
+
+        def __init__(self):
+            dispatcher.connect(self.spider_closed, signals.spider_closed)
+
+
         def parse(self, response):
 
             # Connect to Beanstalkd server
-            #self.beanstalk = beanstalkc.Connection(host=self.host_beanstalkd, port=11301)
+            self.beanstalk = beanstalkc.Connection(host=self.host_beanstalkd, port=11301)
             
             # See all tubes:
-            #self.beanstalk.tubes()
+            self.beanstalk.tubes()
 
             # Switch to the default (tube):
-            #self.beanstalk.use('default')
+            self.beanstalk.use('default')
 
 
             self.makedirResults()
@@ -88,17 +92,21 @@ class MySpider(scrapy.Spider):
                 native_objects[nat_object]['metadata']['upper_node_info'] = {"name":"node", "value":node}
                 native_objects[nat_object]['metadata']['upper_domain_info'] = {"name":"domain", "value":dom}
                 native_objects[nat_object]['metadata']['upper_category_info'] = {"name":"category", "value":statement}
+                native_objects[nat_object]['metadata']['backend'] = {"name":"backend", "value":"mon"}
                 final_object = {}
                 final_object[nat_object] = native_objects[nat_object]
                 json_final_object = json.dumps(final_object)
 
                 print '##########################################################################################'
                 self.createFile(node + '_' + dom + '_' + statement + '_' + nat_object,final_object)
-                #self.beanstalk.put(json_final_object)
+                self.beanstalk.put(json_final_object)
                 print json_final_object
                 print '##########################################################################################'
 
             return
+
+        def spider_closed(self,spider):
+            self.beanstalk.put('quit')
 
 
         def createFile(self,namefile,results):
